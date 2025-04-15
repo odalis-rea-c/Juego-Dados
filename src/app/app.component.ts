10fef10c1d5325d8266c71f9bd8accd7fd9474e4
odalis-rea-c <osxyrea@gmail.com>
2025-04-15 18:26:28 -0500

    Juego de Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Dados';
+
+  dadoIzquierdo:string='/img/dice1.png';
+  dadoDerecho:string='/img/dice1.png';
+
+  numero1!:number;
+  numero2!:number;
+
+  tirarDados(){
+    this.numero1 = Math.round(Math.random() * 6);
+    this.numero2 = Math.round(Math.random() * 6);
+
+    console.log(this.numero1)
+    console.log(this.numero2)
+
+  }
 }
