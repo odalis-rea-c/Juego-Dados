9fb175426814484760ca88c6ff33a472ab0efc90
odalis-rea-c <osxyrea@gmail.com>
2025-04-16 16:05:07 -0500

    Juego funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,14 +12,17 @@ export class AppComponent {
   title = 'Dados';
 
   dadoIzquierdo:string='/img/dice1.png';
-  dadoDerecho:string='/img/dice1.png';
+  dadoDerecho:string='/img/dice4.png';
 
-  numero1!:number;
-  numero2!:number;
+  numero1:number = 1;
+  numero2:number = 2;
 
   tirarDados(){
-    this.numero1 = Math.round(Math.random() * 6);
-    this.numero2 = Math.round(Math.random() * 6);
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+
+    this.dadoIzquierdo = '/img/dice' + this.numero1 + '.png';
+    this.dadoDerecho = '/img/dice' + this.numero2 + '.png';
 
     console.log(this.numero1)
     console.log(this.numero2)
